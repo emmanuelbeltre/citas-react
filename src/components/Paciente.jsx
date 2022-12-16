1e18a687f3f56e65c779ff9bc9424d950ae177d3
emmanuelbeltre <beltrelgg2@gmail.com>
2022-12-15 23:12:54 -0400

    Componente de pacientes creado e implementado

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,29 @@
+const Paciente = () => {
+  return (
+    <div className=" m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+
+    <p className=" font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}
+        <span className=" font-normal normal-case">Loki</span>
+      </p>
+      
+      <p className=" font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}
+        <span className=" font-normal normal-case">Emmanuel</span>
+      </p>
+      
+      <p className=" font-bold mb-3 text-gray-700 uppercase">Email:{' '}
+        <span className=" font-normal normal-case">Emmanuel@gmail.com</span>
+      </p>
+      
+      <p className=" font-bold mb-3 text-gray-700 uppercase">Fwecha Alta:{' '}
+        <span className=" font-normal normal-case">26 Diciembre 2022</span>
+      </p>
+      
+      <p className=" font-bold mb-3 text-gray-700 uppercase">Síntomas:{' '}
+        <span className=" font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptates non laudantium. Nesciunt non animi ad, eos voluptatibus dignissimos ipsam, ipsum eum neque molestiae fuga perferendis, quia exercitationem quibusdam similique.</span>
+      </p>
+              
+   </div>
+  )
+}
+
+export default Paciente
