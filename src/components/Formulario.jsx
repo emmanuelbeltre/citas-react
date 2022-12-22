5b538cec538b36bb103dfd7dce2a7712e8ca5733
emmanuelbeltre <beltrelgg2@gmail.com>
2022-12-21 21:29:45 -0400

    Avances del proyecto
    Edicion y eliminacion de pacientes. Hooks mejorados. Avances con el state

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,102 @@
-import React from 'react'
+import { useEffect, useState} from 'react'
+import Error from './Error';
 
-const Formulario = () => {
+const Formulario = ({pacientes, setPacientes, paciente, setPaciente}) => {
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+  
+  const [error, setError] =  useState(false);
+
+  useEffect(()=>{
+   if ( Object.keys(paciente).length >0){
+    setNombre(paciente.nombre);
+    setPropietario(paciente.propietario);
+    setEmail(paciente.email);
+    setFecha(paciente.fecha);
+    setSintomas(paciente.sintomas);
+   }
+  }, [paciente])
+
+
+
+
+  const generarId = () =>{
+    const random = Math.random().toString(36).substring(2);
+    const fecha = Date.now().toString(36);
+
+    return random + fecha;
+  }
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+    // Validacion del formulario 
+    if([nombre, propietario, email, fecha, sintomas].includes('')){
+      setError(true);
+      return;
+    }
+      setError(false)
+     
+     //Objeto de paciente 
+     const objetoPaciente = {
+      nombre , 
+      propietario, 
+      email, 
+      fecha, 
+      sintomas,
+     }
+
+     if (paciente.id ){
+      //TODO: Eitando registro
+      objetoPaciente.id = paciente.id;
+
+      const pacientesActualizados = pacientes.map(pacienteState => pacienteState.id=== paciente.id 
+        ? objetoPaciente : pacienteState);
+
+        setPacientes(pacientesActualizados);
+        setPaciente({});
+
+     } else{
+      //Nuevo Registro
+      objetoPaciente.id= generarId();
+      setPacientes([...pacientes, objetoPaciente]); 
+     }
+
+     
+
+
+      //Reiniciar Formulario
+      setNombre('');
+      setPropietario('');
+      setEmail('');
+      setFecha('');
+      setSintomas('');
+    
+ 
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className='text-lg mt-5 text-center mb-10'>
         Añade pacientes y {''}
         <span className='text-indigo-600 font-bold'>Administrarlos</span>
       </p>
 
-      <form action="" className=" bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form 
+      onSubmit={handleSubmit}
+      action="" className=" bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+        {error && <Error >
+          <p>
+          Todos los campos son obligatorios
+          </p>
+        </Error>
+      }
       <div className='mb-5'>
             <label htmlFor="mascota" className=' block text-gray-700 uppercase font-bold '>
               Nombre Mascota
@@ -18,7 +105,10 @@ const Formulario = () => {
             id='mascota'
             type="text"
             placeholder='Nombre de la Mascota'
-            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md ' />
+            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md '
+            value={nombre}
+            onChange={ (e) => setNombre(e.target.value) }
+            />
         </div>
      
         <div className='mb-5'>
@@ -29,7 +119,10 @@ const Formulario = () => {
             id='propietario'
             type="text"
             placeholder='Nombre del Propietario'
-            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md ' />
+            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md '
+            value={propietario}
+            onChange={ (e) => setPropietario(e.target.value) }
+             />
         </div>
         
         <div className='mb-5'>
@@ -40,7 +133,10 @@ const Formulario = () => {
             id='email'
             type="email"
             placeholder='Email Contacto Propietartio'
-            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md ' />
+            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md '
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
+            />
         </div>
 
         <div className='mb-5'>
@@ -50,7 +146,10 @@ const Formulario = () => {
             <input
             id='alta'
             type="date"
-            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md ' />
+            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md ' 
+            value={fecha}
+            onChange={ (e) => setFecha(e.target.value) }
+            />
         </div>
 
         <div className='mb-5'>
@@ -59,9 +158,10 @@ const Formulario = () => {
             </label>
            <textarea 
             id="sintomas" 
-            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md ' 
-           
+            className=' border-2  w-full p-2 mt-2 placeholder-gray-400 rounded-md '        
            placeholder='Describe los síntomas' 
+           value={sintomas}
+            onChange={ (e) => setSintomas(e.target.value) }
             />
         
 
@@ -71,7 +171,7 @@ const Formulario = () => {
         
         <input type="submit" 
         className=' bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all'
-        value="Agregar Paciente"
+        value={paciente.id ? 'Editar paciente' : 'Agregar Paciente'}
         />
         
 
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,26 +1,55 @@
-const Paciente = () => {
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
+
+  const {nombre, propietario, email, fecha, sintomas, id} =  paciente;
+
+  const handleEliminar = () =>{
+    const respuesta = confirm('¿Deseas eliminar este paciente?');
+    if (respuesta){
+      eliminarPaciente(id)
+    }
+  }
+
+
   return (
-    <div className=" m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className=" x-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
 
     <p className=" font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}
-        <span className=" font-normal normal-case">Loki</span>
+        <span className=" font-normal normal-case">{nombre}</span>
       </p>
       
       <p className=" font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}
-        <span className=" font-normal normal-case">Emmanuel</span>
+        <span className=" font-normal normal-case">{propietario}</span>
       </p>
       
       <p className=" font-bold mb-3 text-gray-700 uppercase">Email:{' '}
-        <span className=" font-normal normal-case">Emmanuel@gmail.com</span>
+        <span className=" font-normal normal-case">{email}</span>
       </p>
       
       <p className=" font-bold mb-3 text-gray-700 uppercase">Fwecha Alta:{' '}
-        <span className=" font-normal normal-case">26 Diciembre 2022</span>
+        <span className=" font-normal normal-case">{fecha}</span>
       </p>
       
       <p className=" font-bold mb-3 text-gray-700 uppercase">Síntomas:{' '}
-        <span className=" font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptates non laudantium. Nesciunt non animi ad, eos voluptatibus dignissimos ipsam, ipsum eum neque molestiae fuga perferendis, quia exercitationem quibusdam similique.</span>
+        <span className=" font-normal normal-case">{sintomas}</span>
       </p>
+
+      <div className=" flex justify-between mt-10">
+        <button 
+        type="button"
+        className=" py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        onClick={()=> setPaciente(paciente)}
+        >
+          Editar
+        </button>
+
+        <button 
+        type="button"
+        className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        onClick={handleEliminar}
+        >
+        Eliminar
+        </button>
+      </div>
               
    </div>
   )
