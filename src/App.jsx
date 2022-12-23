ae69da74bcc0eddfd83d0b994230b92b09997f5d
emmanuelbeltre <beltrelgg2@gmail.com>
2022-12-23 14:54:42 -0400

    Proyecto completado
    Memoria cargada al local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) || []);
   const [paciente, setPaciente]= useState({});
+
+  // useEffect(()=>{
+  //   const obtenerLocalStorage = () =>{
+  //     const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+  //    setPacientes(pacientesLS); 
+  //   }
+  //   obtenerLocalStorage();
+  // }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes])
+
   const eliminarPaciente = (id) =>{
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
     setPacientes(pacientesActualizados)
